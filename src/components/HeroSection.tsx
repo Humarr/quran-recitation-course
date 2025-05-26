6117705cbf82f9db3ce82c81a8bd5707237e4e42
Humarr <74363540+Humarr@users.noreply.github.com>
2025-05-26 23:17:21 +0100

    Add checkout system

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,8 +1,8 @@
 import CTAButton from './CTAButton';
 import Image from 'next/image';
-
+import { useRouter } from 'next/router';
 export default function HeroSection() {
-
+    const router = useRouter();
       const handleCheckout = () => {
     // You can add pre-checkout logic here
     router.push('/checkout');
